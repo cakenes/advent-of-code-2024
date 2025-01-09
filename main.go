af6b9e7eed7c884837e5c35da5679a949fa3778e
cakenes <jyri.kurtakko@gmail.com>
2025-01-09 17:16:22 +0200

    Add day2 solution

--- main.go
+++ main.go
@@ -11,10 +11,11 @@ import (
 
 func main() {
 	day1()
+	day2()
 }
 
-func day1() {
-	file, err := os.Open("day1.txt")
+func read(filename string) []string {
+	file, err := os.Open(filename)
 
 	if err != nil {
 		panic(err)
@@ -22,12 +23,22 @@ func day1() {
 
 	defer file.Close()
 
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
+
+func day1() {
+	lines := read("day1.txt")
+
 	var first []int
 	var second []int
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
+	for _, line := range lines {
 		numbers := strings.Fields(line)
 		if len(numbers) >= 2 {
 			var tmpfirst, tmpsecond int
@@ -47,5 +58,43 @@ func day1() {
 		final += int(math.Abs(float64(first[i] - second[i])))
 	}
 
+	fmt.Println(final)
+}
+
+func day2() {
+	lines := read("day2.txt")
+
+	var lineNumbers [][]int
+	final := 0
+
+	for _, line := range lines {
+		var numbers []int
+		fields := strings.Fields(line)
+		for _, field := range fields {
+			var tmp int
+			fmt.Sscanf(field, "%d", &tmp)
+			numbers = append(numbers, tmp)
+		}
+		lineNumbers = append(lineNumbers, numbers)
+	}
+
+	for _, numbers := range lineNumbers {
+		increasing := false
+		decreasing := false
+
+		for i := 1; i < len(numbers); i++ {
+			diff := numbers[i] - numbers[i-1]
+			if diff != 1 && diff != 2 && diff != 3 {
+				increasing = true
+			}
+			if diff != -1 && diff != -2 && diff != -3 {
+				decreasing = true
+			}
+		}
+
+		if (increasing || decreasing) && !(increasing && decreasing) {
+			final++
+		}
+	}
 	fmt.Println(final)
 }
